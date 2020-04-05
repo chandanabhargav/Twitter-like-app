85655a5079795bd24197e3aa8df1450132ea12c2
chandanabhargav <chandanabhargav29@gmail.com>
2020-04-05 15:50:39 +0530

    package.json changes

--- database/db.js
+++ database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost/twitter'
+    //db: 'mongodb://localhost/twitter'
+    db: 'mongodb://chandanabhargav:bhargavchan999@ds019976.mlab.com:19976/twitter-sample9'
 }
